070711d80a7a8365743492ac52d8c45175182b08
Dwire <gjdwyer8@gmail.com>
2019-12-19 18:05:54 -0500

    added simple validation

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,32 @@
 class EmployeesController < ApplicationController
+  def index 
+    @employees = Employee.all
+  end 
+
+  def show
+    @employee = Employee.find(params[:id])
+  end 
+
+  def new
+    @employee = Employee.new
+  end
+  
+  def create 
+    employee = Employee.create(employee_params)
+
+    if employee.valid? 
+      redirect_to employee_path(employee)
+    else 
+      flash[:messages] = employee.errors.full_messages
+      redirect_to new_employee_path
+    end 
+
+  end
+  
+  private 
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :office, :alias, :title, :img_url, :dog_id)
+  end 
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,11 @@
 class Employee < ApplicationRecord
+  belongs_to :dog
+
+  validates :alias, presence: true
+
+
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end 
 end
